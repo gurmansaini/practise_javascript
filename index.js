2a48ddcb8afc2a38c7fdf62f92647aa6f6f5f34b
gurmansaini <gurmansaini1@gmail.com>
2020-08-04 18:39:20 +0530

    js console api

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 //console.log("Hello World");
 //alert("me");
 //document.write("this is gurman");
+
+//2.Javascript console API
+console.log("hello people!", 22 + 1, "is my age ");
+console.warn(
+  "Its a warning, dont ignore it otherwise you will have to handle drama later"
+);
+console.error(
+  "this is an error, but its ok we all make errors and learn, bcz we are humans"
+);
