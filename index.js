e9c31377dc22e8ef14c005d3b43ba65f66587d9f
gurmansaini <gurmansaini1@gmail.com>
2020-08-13 01:28:11 +0530

    JS Variables

--- index.js
+++ index.js
@@ -4,10 +4,53 @@
 //document.write("this is gurman");
 
 //2.Javascript console API
-console.log("hello people!", 22 + 1, "is my age ");
+/*console.log("hello people!", 22 + 1, "is my age ");
 console.warn(
   "Its a warning, dont ignore it otherwise you will have to handle drama later"
 );
 console.error(
   "this is an error, but its ok we all make errors and learn, bcz we are humans"
-);
+);*/
+
+/* 
+hack: In VS code we can comment multiple lines by selecting them and press (ctrl+/) 
+*/
+
+/* 3. Javascript Variables
+What are Variables?- Containers to store data values
+*/
+
+var number1 = 20;
+var number2 = 50;
+console.log(number1 + number2);
+
+//Data types in JS
+//Numbers
+
+var num1 = 455;
+var num2 = 33.88;
+
+//Strings
+var str1 = "Oh man, this is string";
+var str2 = "This is also String!!!!!!!!!!!!!!!!!!!";
+
+//Objects
+var marks = {
+  Gaurav: 29,
+  Lilly: 98,
+  Harman: 67.998,
+};
+console.log(marks);
+
+//Booleans
+var a = true;
+var b = false;
+console.log(a, b);
+
+//var und = undefined;
+var und;
+console.log(und);
+
+//Null
+var n = null;
+console.log(n);
